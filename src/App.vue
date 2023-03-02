dd5add878f390db2096027a2afc1277283df7d27
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-03-01 21:20:59 -0500

    script setup and name functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup>
+  import { ref, onMounted, computed, watch } from 'vue'
 
+  const items = ref([])
+  const name = ref('')
+
+  const input_content = ref('')
+  const input_category = ref(null)
+
+  const items_asc = computed(() => todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt
+  }))
+
+  watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+  })
+
+  onMounted(() => {
+    name.value = localStorage.getItem('name') || ''
+  })
 </script>
 
 <template>
-  <h1>Todo App</h1>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Hello, <input type="text" placeholder="_" v-model="name">
+      </h2>
+    </section>
+  </main>
 </template>
